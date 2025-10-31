156f2e28b9a9ac1ce13427d0d5d0f483f8330d09
lob0scuro <kamrin717@gmail.com>
2025-10-31 11:43:57 -0500

    did it!

--- client/src/routes/Auth/SendLink/SendLink.jsx
+++ client/src/routes/Auth/SendLink/SendLink.jsx
@@ -1,5 +1,5 @@
 import styles from "./SendLink.module.css";
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
