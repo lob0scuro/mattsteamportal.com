bda7b3a468045c3510cb1871b3ec653b84823a1e
lob0scuro <kamrin717@gmail.com>
2026-01-12 14:25:12 -0600

    added functionality for PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 const SERVER = process.env.VITE_SERVER_URL;
 
@@ -12,6 +13,34 @@ export default defineConfig(({ mode }) => {
       react({
         babel: {
           plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "portal-logo-512.png",
+          "portal-logo-192.png",
+          "portal-logo-apple-icon.png",
+        ],
+        manifest: {
+          name: "Matt's Team Portal",
+          short_name: "TeamPortal",
+          display: "standalone",
+          start_url: "/",
+          background_color: "#2a2829",
+          theme_color: "#262f4c",
+          icons: [
+            {
+              src: "/portal-logo-192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/portal-logo-512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
         },
       }),
     ],
